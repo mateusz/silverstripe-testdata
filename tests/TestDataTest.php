374b3dc689726c0350f42848b04f8046109f3a80
Mateusz Uzdowski <mateusz@silverstripe.com>
2011-11-14 10:49:29 +1300

    MINOR add test for circular references

--- tests/TestDataTest.php
+++ tests/TestDataTest.php
@@ -4,6 +4,11 @@ class TestDataTest extends FunctionalTest {
 
 	public $envType;
 
+	protected $extraDataObjects = array(
+		'YamlFixtureTest_CircularOne',
+		'YamlFixtureTest_CircularTwo'
+	);
+
 	function setUp() {
 		parent::setUp();
 		TestDataController::$data_dir = 'testdata/tests';
@@ -74,4 +79,48 @@ class TestDataTest extends FunctionalTest {
 			array('URLSegment'=>'baseline')
 		), $all);
 	}
+
+	function testCircularReferences() {
+		$circularOne = DataObject::get_by_id("TestDataTest_CircularOne", $this->idFromFixture("TestDataTest_CircularOne", "one"));
+		$circularTwo = DataObject::get_by_id("TestDataTest_CircularTwo", $this->idFromFixture("TestDataTest_CircularTwo", "two"));
+
+		$this->assertEquals($circularOne->HasOneID, $circularTwo->ID, "Circular references in has_one work");
+		$this->assertEquals($circularOne->ID, $circularTwo->HasOneID, "Circular references in has_one work");
+
+		$this->assertEquals($circularOne->HasMany()->First()->ID, $circularTwo->ID, "Circular references in has_many work");
+		$this->assertEquals($circularOne->ID, $circularTwo->HasMany()->First()->ID, "Circular references in has_many work");
+
+		$this->assertEquals($circularOne->ManyMany()->First()->ID, $circularTwo->ID, "Circular references in many_many work");
+		$this->assertEquals($circularOne->ID, $circularTwo->ManyMany()->First()->ID, "Circular references in many_many work");
+	}
+}
+
+class TestDataTest_CircularOne extends DataObject implements TestOnly {
+	static $db = array(
+		"Name" => "Varchar"
+	);
+	static $has_one = array(
+		"HasOne" => "TestDataTest_CircularTwo",
+	); 
+	static $has_many = array(
+		"HasMany" => "TestDataTest_CircularTwo"
+	);
+	static $many_many = array(
+		"ManyMany" => "TestDataTest_CircularTwo"
+	);
+}
+
+class TestDataTest_CircularTwo extends DataObject implements TestOnly {
+	static $db = array(
+		"Name" => "Varchar"
+	);
+	static $has_one = array(
+		"HasOne" => "TestDataTest_CircularOne",
+	); 
+	static $has_many = array(
+		"HasMany" => "TestDataTest_CircularOne"
+	);
+	static $belongs_many_many = array(
+		"ManyMany" => "TestDataTest_CircularOne"
+	);
 }
