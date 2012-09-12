2e7b09d647005dabd2a6b9e10e830c601d4218ec
Mateusz Uzdowski <mateusz@silverstripe.com>
2012-09-12 16:31:28 +1200

    Move routes to config system.

--- _config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Director::addRules(100, array('dev/data/export//$Action/$ID/$OtherID' => 'TestDataExporter'));
-Director::addRules(100, array('dev/data//$Action/$ID/$OtherID' => 'TestDataController'));
